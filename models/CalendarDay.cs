1b6ffe499bdec8ec7b34c620f6ade0cbf8701fe0
chn02742 <61205141+chn02742@users.noreply.github.com>
2022-07-29 15:57:43 -0500

    Create CalendarDay.cs

---
+++ models/CalendarDay.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerOutlook.Models
+{
+    public class CalendarDay
+    {
+        public int DayNumber { get; set; }
+        public DateTime Date { get; set; }
+        public bool IsEmpty { get; set; }
+        public List<CalendarEvent> Events { get; set; }
+    }
+}
