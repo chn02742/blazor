4b31965c157c57e58634b284178e6a8b0d626d2b
Choi Ng <61205141+chn02742@users.noreply.github.com>
2023-03-31 16:06:46 -0500

    Create InputAddress.cs

---
+++ models/InputAddress.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace NewEra.PolicyholderV2.Models
+{
+	public class InputAddress
+	{
+		[Required]
+		public string AddressL1 { get; set; }
+		public string AddressL2 { get; set; }
+		public string AddressL3 { get; set; }
+		[Required]
+		public string City { get; set; }
+		[Required]
+		public string State { get; set; }
+		[Required]
+		[Display(Name = "Country")]
+		public string Country { get; set; }
+		[Required]
+		[Display(Name = "Postal Code")]
+		public string PostalCode { get; set; }
+	}
+}
